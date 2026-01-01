3c2ba99277138f2be55d7e82497cdbcec2601ed6
bhrdwjuddhv <bhardwajuddhav2@gmail.com>
2026-01-02 01:12:35 +0530

    Added Head, footer and hero section

--- frontend/brustOut/src/App.jsx
+++ frontend/brustOut/src/App.jsx
@@ -1,36 +1,32 @@
-import { Button } from "@heroui/react";
-
-function App() {
+import React from "react";
+import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx";
+export default function HeroSection() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-900 to-purple-900">
-      <div className="bg-white/10 backdrop-blur-xl p-10 rounded-3xl shadow-2xl border border-white/20 text-center">
-        <h1 className="text-4xl font-extrabold text-white mb-4 tracking-wide">
-          HeroUI Button 🚀
-        </h1>
+      <>
+      <div>
+        <Header />
+          <div>
+          <div className="flex flex-col items-center justify-center w-full gap-10 m-10">
 
-        <p className="text-gray-300 mb-8">
-          Clean • Modern • Responsive
-        </p>
+          <div className="h-full text-center text-4xl lg:text-6xl px-10">
+              You don't have to face it alone
+          </div>
+              <div className="text-center">
+                  Connect with empathetic mentors anonymously. Share your feelings in a safe, judgment-free space whenever you need support.
+              </div>
+              <div className="flex flex-col lg:flex-row items-center justify-center w-full gap-4">
+                  <button>Start Talking Now</button>
+                  <button>Learn More &rarr;</button>
+              </div>
+          </div>
+              <div className="flex flex-col lg:flex-row items-center justify-center w-full gap-4">
+                  <p className="text-5xl">How it works?</p>
 
-        <Button
-          color="primary"
-          size="lg"
-          className="
-            px-10 py-6
-            text-lg font-bold
-            rounded-2xl
-            shadow-[0_0_30px_rgba(99,102,241,0.6)]
-            hover:scale-105
-            active:scale-95
-            transition-all
-            duration-300
-          "
-        >
-          Click Me ✨
-        </Button>
+              </div>
+          </div>
+          <Footer />
       </div>
-    </div>
+      </>
   );
 }
-
-export default App;
